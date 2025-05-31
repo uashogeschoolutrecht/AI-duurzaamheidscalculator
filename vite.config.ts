a283e895f1ea1a5ebe10176c3834478fa566ca2c
Saddik212 <74911324+Saddik212@users.noreply.github.com>
2025-05-31 20:05:07 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Duurzaamheidstool",
+  base: "/DuurzaamheidstoolV2",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
